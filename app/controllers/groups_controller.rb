3b0acab8b503707707424d0881c1727d5a84e33c
nitro <xiaoren8688@yahoo.co.jp>
2019-05-30 21:53:57 +0900

    update_search

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -32,6 +32,7 @@ class GroupsController < ApplicationController
     params.require(:group).permit(:name, { :user_ids => [] })
   end
   
+  
   def set_group
     @group = Group.find(params[:id])
   end

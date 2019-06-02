1f6cf704d3ebcbb8f9ccdb85adf65c7c02da50b7
nitro <xiaoren8688@yahoo.co.jp>
2019-06-02 20:38:58 +0900

    update_grateup

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -6,10 +6,9 @@ class Api::MessagesController < ApplicationController
       @message = Message.new
       @messages = @group.messages.includes(:user)
       respond_to do |format| 
-        format.html # html形式でアクセスがあった場合は特に何もなし(@messages = Message.allして終わり）
+        format.html 
         format.json { @new_message = @messages.where('id > ?', params[:message][:id]) } # json形式でアクセスがあった場合は、params[:message][:id]よりも大きいidがないかMessageから検索して、@new_messageに代入する
       end
-      # binding.pry
     end
   end
 
